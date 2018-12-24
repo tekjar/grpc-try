22562ef255fb61bcd8304fa84b75223e8c6e895d
raviteja <ravi.teja@atherenergy.com>
2018-12-24 16:14:24 +0530

    Add in app profiler

--- client-go/go.mod
+++ client-go/go.mod
@@ -1,6 +1,7 @@
 module client
 
 require (
+	github.com/akhenakh/statgo v0.0.0-20171021021904-3ae2cda264c5
 	github.com/golang/protobuf v1.2.0
 	golang.org/x/net v0.0.0-20180826012351-8a410e7b638d
 	google.golang.org/grpc v1.17.0
--- client-go/main.go
+++ client-go/main.go
@@ -3,9 +3,11 @@ package main
 import (
 	"client/helloworld"
 	"context"
+	"fmt"
 	"log"
 	"time"
 
+	profile "github.com/akhenakh/statgo"
 	"google.golang.org/grpc"
 )
 
@@ -13,6 +15,34 @@ const (
 	address     = "localhost:50051"
 	defaultName = "world"
 )
+
+func profileSystem(exit chan bool) {
+	ticker := time.NewTicker(time.Second * 10).C
+	stats := profile.NewStat()
+
+	cpu := stats.CPUStats()
+	fmt.Println("1 = ", cpu.LoadMin1, " 5 = ", cpu.LoadMin5, " 15 = ", cpu.LoadMin15)
+	memory := []int{}
+
+	for {
+		select {
+		case <-ticker:
+			mem := stats.MemStats()
+			cpu := stats.CPUStats()
+			fmt.Println("1 = ", cpu.LoadMin1, " 5 = ", cpu.LoadMin5, " 15 = ", cpu.LoadMin15)
+			memory = append(memory, mem.Used)
+		case <-exit:
+			cpu := stats.CPUStats()
+			var totalMem int
+			for _, value := range memory {
+				totalMem += value
+			}
+			fmt.Println("1 = ", cpu.LoadMin1, " 5 = ", cpu.LoadMin5, " 15 = ", cpu.LoadMin15)
+			fmt.Println("average system memory = ", totalMem/len(memory))
+			return
+		}
+	}
+}
 
 func main() {
 	// Set up a connection to the server.
@@ -25,16 +55,22 @@ func main() {
 
 	// Contact the server and print out its response.
 	name := defaultName
+	exit := make(chan bool, 1)
 
+	go profileSystem(exit)
 	for i := 0; i < 10000; i++ {
 		ctx, cancel := context.WithTimeout(context.Background(), time.Second)
 		defer cancel()
 		r, err := c.SayHello(ctx, &helloworld.HelloRequest{Name: name})
+		_ = r
 		if err != nil {
 			log.Fatalf("could not greet: %v", err)
 		}
 
-		log.Printf("Greeting: %s", r.A)
+		// log.Printf("Greeting: %s", r.A)
 		time.Sleep(100 * time.Millisecond)
 	}
+
+	exit <- true
+	time.Sleep(3 * time.Second)
 }
